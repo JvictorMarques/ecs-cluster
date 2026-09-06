9ce5abc70074c77cf6553799559dd7c88b9f3ad7
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-09-06 12:07:30 -0300

    Add GitHub Actions workflow and IAM role for ECS deployment

---
+++ terraform/iam.role.github-actions.tf
@@ -0,0 +1,93 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "ecs-cluster-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:JvictorMarques/ecs-cluster:*"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "ecs-cluster-github-actions-role-policy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "GetAuthorizationToken",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:GetAuthorizationToken"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "AllowPushPull",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ],
+        "Resource" : "${aws_ecr_repository.this.arn}"
+      },
+      {
+        "Sid" : "RegisterTaskDefinition",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecs:RegisterTaskDefinition",
+          "ecs:DescribeTaskDefinition"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "PassRolesInTaskDefinition",
+        "Effect" : "Allow",
+        "Action" : [
+          "iam:PassRole"
+        ],
+        "Resource" : [
+          "${aws_iam_role.ecs_execution_task_definition.arn}"
+        ]
+      },
+      {
+        "Sid" : "DeployService",
+        "Effect" : "Allow",
+        "Action" : [
+          "ecs:UpdateService",
+          "ecs:DescribeServices"
+        ],
+        "Resource" : [
+          "${aws_ecs_service.this.arn}"
+        ]
+      }
+    ]
+  })
+}
